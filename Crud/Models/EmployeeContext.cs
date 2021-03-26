38154d7e39191340f39afaab2924c5adb7ff4571
DomaskinDA <DomaskinDA@elcomplus.ru>
2021-03-26 09:48:45 +0700

    Add project files.

---
+++ Crud/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+    }
+}
